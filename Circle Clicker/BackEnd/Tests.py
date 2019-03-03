9b9ef22ada6de66e8de7f83058cc992300fa446b
malpus <max.paikin@gmail.com>
2019-03-03 17:50:24 -0500

    Some more

--- Circle Clicker/BackEnd/Tests.py
+++ Circle Clicker/BackEnd/Tests.py
@@ -3,6 +3,5 @@ from Player import updateLocation
 
 class testAllMethods(unittest.TestCase):
     def testUpdateLocation(self):
-        p = Player("Max", 0, 1, 10 )
-        p.updateLocation(2,3)
+        Player.updateLocation(2,3)
         self.assertEqual(p.posX, 2)
