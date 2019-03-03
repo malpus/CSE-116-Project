c0cfc1cd9771e8b79401d601e4550d4aa7c696a8
malpus <max.paikin@gmail.com>
2019-03-03 17:24:26 -0500

    Update Player.py

---
+++ Circle Clicker/Back End/Player.py
@@ -0,0 +1,7 @@
+class Circle(name, posX, posY, amount):
+    def updateLocation(x,y):
+        posx = x
+        posy = y
+    def checkElimination(time, a):
+        if (time < 0 and self.amount > 10):
+            self.amount = 0
